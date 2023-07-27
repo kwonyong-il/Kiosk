5c100426893eb4306200086c231dd25233705894
kwonyongil <kkwonyi95@gmail.com>
2023-07-27 16:18:01 +0900

    git commit -m "lv.3 까지는 완성한듯?"

--- app/src/main/java/com/example/kiosk/kiosk.kt
+++ app/src/main/java/com/example/kiosk/kiosk.kt
@@ -1,22 +1,26 @@
 package com.example.kiosk
 
-import com.example.kiosk_final.Beer
-import com.example.kiosk_final.Burger
-import com.example.kiosk_final.Drink
-import com.example.kiosk_final.FrozenCustard
-import com.example.kiosk_final.beerList
-import com.example.kiosk_final.burgerList
-import com.example.kiosk_final.drinkList
-import com.example.kiosk_final.frozenCustardList
+
+import Order
+import com.example.kiosk_final.beer
+import com.example.kiosk_final.burger
+import com.example.kiosk_final.drink
+import com.example.kiosk_final.frozenCustard
+import com.example.kiosk_final.processPayment
+
 
 fun main() {
-    val menuName: ArrayList<String> = arrayListOf("1.Burger", "2.Frozen Custard", "3.Drinks", "4.Beer")
+    val menuName: ArrayList<String> =
+        arrayListOf("1.Burger", "2.Frozen Custard", "3.Drinks", "4.Beer","5.결제")
     val menuDisplay: ArrayList<String> = arrayListOf(
         "앵거스 비프 통살을 다져만든 버거",
         "매장에서 신선하게 만드는 아이스크림",
         "매장에서 직접 만드는 음료",
-        "뉴욕 브루클린 브루어리에서 양조한 맥주"
+        "뉴욕 브루클린 브루어리에서 양조한 맥주",
+        "주문 내역 확인 및 결제"
     )
+    val order = Order()
+
     println("Shake Shack 버거 에 오신걸 환영합니다.")
     println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.")
     println("")
@@ -32,19 +36,11 @@ fun main() {
     while (true) {
         var selectNumber = readLine()?.toIntOrNull()
         when (selectNumber) {
-            1 -> { val burger = Burger()
-                burgerList(burger)
-            }
-            2 -> { val frozenCustard = FrozenCustard()
-                frozenCustardList(frozenCustard)
-            }
-            3 -> { val drink = Drink()
-                drinkList(drink)
-            }
-            4 -> {val beer = Beer()
-                beerList(beer)
-            }
-
+            1 -> burger()
+            2 -> frozenCustard()
+            3 -> drink()
+            4 -> beer()
+            5 -> processPayment(order)
             0 -> break
             else ->
                 println("잘못 입력하셨습니다. 메뉴에 있는 번호를 입력해주세요.")
