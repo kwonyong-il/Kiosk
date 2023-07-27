571f6a9cdeeb10e18085c8cd959e65e0aaf01db5
sunho_jeong <sunho512@gmail.com>
2023-07-27 16:21:19 +0900

    정의 브랜치 입니다.

--- app/src/main/java/com/example/kiosk/kiosk.kt
+++ app/src/main/java/com/example/kiosk/kiosk.kt
@@ -1,110 +1,73 @@
-package com.example.kiosk
+import java.math.BigDecimal
 
 fun main() {
-    println("7 TEAM 버거 에 오신걸 환영합니다.")
-    println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.")
-    println("")
-    println("--------------------[7 TEAM 버거}--------------------")
-    println("1.Burger ㅣ 앵거스 비프 통살을 다져만든 버거")
-    println("2.Frozen Custrad ㅣ 매장에서 신선하게 만드는 아이스크림")
-    println("3.Drinks ㅣ매장에서 직접 만드는 음료")
-    println("4.Bear ㅣ 뉴욕 브루클린 브루어리에서 양조한 맥주")
-    println("")
-    println("0. 종료")
-    println("")
-    println("번호를 입력해 주세요")
-
+    val menu = Menu()
+    val paymentManager = PaymentManager(BigDecimal("100000"))
+    val order = Order()
 
     while (true) {
-        var selectNumber = readLine()?.toIntOrNull()
+        println("[ SHAKESHACK MENU ]")
+        println("1. Burgers         | 앵거스 비프 통살을 다져만든 버거")
+        println("2. Forzen Custard  | 매장에서 신선하게 만드는 아이스크림")
+        println("3. Drinks          | 매장에서 직접 만드는 음료")
+        println("4. Beer            | 뉴욕 브루클린 브루어리에서 양조한 맥주")
+        println("5. 결제하기         | 주문한 메뉴를 결제합니다.")
+        println("0. 종료            | 프로그램 종료")
 
-        when (selectNumber) {
+        print("메뉴를 선택해주세요: ")
+        val selectedMenu = readLine()?.toIntOrNull() ?: -1
+
+        when (selectedMenu) {
+            0 -> {
+                println("프로그램을 종료합니다.")
+                return
+            }
             1 -> {
-                val burger = Burger()
-                burger.selectBurgerNumber()
+                menu.showMenu(menu.menuItems.filterIsInstance<Burger>())
+                while (true) {
+                    val selectedBurger = menu.selectMenu(menu.menuItems.filterIsInstance<Burger>())
+                    if (selectedBurger == 0) break
+                    if (selectedBurger in 1..menu.menuItems.filterIsInstance<Burger>().size) {
+                        val burger = menu.menuItems.filterIsInstance<Burger>()[selectedBurger - 1]
+                        order.addItem(burger, paymentManager)
+                    } else {
+                        println("유효하지 않은 메뉴 번호입니다. 다시 선택해주세요.")
+                    }
+                }
             }
-
             2 -> {
-                val frozenCustard = FrozenCustard()
-                frozenCustard.selectFrozenCustardNumber()
+                menu.showMenu(menu.menuItems.filterIsInstance<IceCream>())
+                while (true) {
+                    val selectedCustard = menu.selectMenu(menu.menuItems.filterIsInstance<IceCream>())
+                    if (selectedCustard == 0) break
+                    if (selectedCustard in 1..menu.menuItems.filterIsInstance<IceCream>().size) {
+                        val custard = menu.menuItems.filterIsInstance<IceCream>()[selectedCustard - 1]
+                        order.addItem(custard, paymentManager)
+                    } else {
+                        println("유효하지 않은 메뉴 번호입니다. 다시 선택해주세요.")
+                    }
+                }
             }
-
             3 -> {
-                val drinks = Drinks()
-                drinks.selectDrinksNumber()
+                menu.showMenu(menu.menuItems.filterIsInstance<Drink>())
+                while (true) {
+                    val selectedDrink = menu.selectMenu(menu.menuItems.filterIsInstance<Drink>())
+                    if (selectedDrink == 0) break
+                    if (selectedDrink in 1..menu.menuItems.filterIsInstance<Drink>().size) {
+                        val drink = menu.menuItems.filterIsInstance<Drink>()[selectedDrink - 1]
+                        order.addItem(drink, paymentManager)
+                    } else {
+                        println("유효하지 않은 메뉴 번호입니다. 다시 선택해주세요.")
+                    }
+                }
             }
-
             4 -> {
-                val beer = Beer()
-                beer.selectBeerNumber()
+                // 이전 코드와 동일 (Beer 메뉴 처리)
             }
-
-            0 -> break
-            else ->
-                println("잘못 입력하셨습니다. 메뉴에 있는 번호를 입력해주세요.")
+            5 -> {
+                order.checkOut(paymentManager) // 주문 확인 및 결제 처리
+            }
+            else -> println("유효하지 않은 메뉴 번호입니다. 다시 선택해주세요.")
         }
-    }
-
-}
-
-class Burger() {
-    fun selectBurgerNumber() {
-        println("--------------------[Burgers}--------------------")
-        println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거")
-        println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거")
-        println("3. Shroom Burger | W 9.4 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거")
-        println("4. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거")
-        println("")
-        println("0. 뒤로 가기 ㅣ 메인 화면으로 가기")
-        var selectBurgerNumber = readLine()?.toIntOrNull()
-
-        if (selectBurgerNumber == 0)
-            return main()
-    }
-}
-
-class FrozenCustard() {
-    fun selectFrozenCustardNumber() {
-        println("--------------------[Frozen Custard}--------------------")
-        println("1. Shake   | W 5.9 | 바닐라,초콜렛,솔티드카라멜,블랙&화이트 ")
-        println("2. Cups&Cones   | W 4.9  | 바닐라,초콜렛 ")
-        println("3. Concrete | W 5.9 | 쉐이크쉑의 쫀득한 커스터드와 다양한 믹스-인 조합")
-        println("")
-        println("0. 뒤로 가기 ㅣ 메인 화면으로 가기")
-        var selectFrozenCustardNumber = readLine()?.toIntOrNull()
-
-        if (selectFrozenCustardNumber == 0)
-            return main()
-    }
-}
-
-class Drinks() {
-    fun selectDrinksNumber() {
-        println("--------------------[Drinks}--------------------")
-        println("1. Lemonade   | W 3.9 | 매장에서 직접 만드는 상큼한 레몬에이드")
-        println("2. IceTea    | W 3.4 | 직접 유기농 홍차를 우려낸 아이스티")
-        println("3. Fifty/Fifty | W 3.5 | 레몬에이드와 아이스티의 만남")
-        println("4. Fountain Soda  | W 3.7 | 코카콜라,스프라이트,환타")
-        println("")
-        println("0. 뒤로 가기 ㅣ 메인 화면으로 가기")
-        var selectDrinksNumber = readLine()?.toIntOrNull()
-
-        if (selectDrinksNumber == 0)
-            return main()
-    }
-}
-
-class Beer() {
-    fun selectBeerNumber() {
-        println("--------------------[Beer}--------------------")
-        println("1. ShackMeister Ale   | W 9.8 | 쉐이크쉑의 버거를 위해 특별히 양조한 에일 맥주")
-        println("2. Magpie Brewing Co.   | W 6.8 ")
-        println("3. The Hand and Malt | W 6.8  ")
-        println("")
-        println("0. 뒤로 가기 ㅣ 메인 화면으로 가기")
-        var selectBeerNumber = readLine()?.toIntOrNull()
-
-        if (selectBeerNumber == 0)
-            return main()
     }
 }
