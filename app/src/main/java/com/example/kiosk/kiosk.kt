70c65849e4ff20eac4687509b0e867f34656e802
kwonyongil <kkwonyi95@gmail.com>
2023-07-27 16:44:33 +0900

    git commit -m "이번이 진짜 마지막"

--- app/src/main/java/com/example/kiosk/kiosk.kt
+++ app/src/main/java/com/example/kiosk/kiosk.kt
@@ -1,106 +1,50 @@
 package com.example.kiosk
 
+import kotlin.system.exitProcess
+
 fun main() {
-    println("7 TEAM 버거 에 오신걸 환영합니다.")
-    println("아래 메뉴판을 보시고 메뉴를 골라 입력해주세요.")
-    println("")
-    println("--------------------[7 TEAM 버거}--------------------")
-    println("1.Burger ㅣ 앵거스 비프 통살을 다져만든 버거")
-    println("2.Frozen Custrad ㅣ 매장에서 신선하게 만드는 아이스크림")
-    println("3.Drinks ㅣ매장에서 직접 만드는 음료")
-    println("4.Bear ㅣ 뉴욕 브루클린 브루어리에서 양조한 맥주")
-    println("")
-    println("0. 종료")
-    println("")
-    println("번호를 입력해 주세요")
+    println("어서오세요, SHAKE SHACK BURGER 입니다.")
 
+    val orderManager = OrderManager()
 
+    inputMyInfo(orderManager)
+}
+
+fun inputInfo(type:String): Int {
     while (true) {
-        var selectNumber = readLine()?.toIntOrNull()
+        try {
+            println("메뉴를 선택 하세요.")
+            println("[ SHAKE SHACK MENU ]")
+            println("[1] Burgers         | 앵거스 비프 통살을 다져 만든 버거")
+            println("[2] Frozen Custard  | 매장에서 신선하게 만드는 아이스크림")
+            println("[3] Drinks          | 매장에서 직접 만드는 음료")
+            println("[4] Beers           | 뉴욕 브루클린 브루어리에서 양조한 맥주")
+            println("[0] 종료             | 프로그램 종료")
 
-        when (selectNumber) {
-            1 -> {
-                val burger = Burger()
-                burger.selectBurgerNumber()
+            val selectNumber = readLine()!!.toIntOrNull()
+            if (selectNumber in 0..4) {
+                return selectNumber ?: 0
+            } else {
+                println("올바른 메뉴 번호를 선택 하세요.")
             }
-
-            2 -> {
-                val frozenCustard = FrozenCustard()
-                frozenCustard.selectFrozenCustardNumber()
-            }
-
-            3 -> {
-                val drinks = Drinks()
-                drinks.selectDrinksNumber()
-            }
-
-            4 -> {
-                val beer = Beer()
-                beer.selectBeerNumber()
-            }
-
-            0 -> break
-            else ->
-                println("잘못 입력하셨습니다. 메뉴에 있는 번호를 입력해주세요.")
+        } catch (e: Exception) {
+            println("올바른 메뉴 번호를 선택 하세요.")
         }
     }
-
 }
-class Burger() {
-    fun selectBurgerNumber() {
-        println("--------------------[Burgers}--------------------")
-        println("1. ShackBurger   | W 6.9 | 토마토, 양상추, 쉑소스가 토핑된 치즈버거")
-        println("2. SmokeShack    | W 8.9 | 베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거")
-        println("3. Shroom Burger | W 9.4 | 몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거")
-        println("4. Cheeseburger  | W 6.9 | 포테이토 번과 비프패티, 치즈가 토핑된 치즈버거")
-        println("")
-        println("0. 뒤로 가기 ㅣ 메인 화면으로 가기")
-        var selectBurgerNumber = readLine()?.toIntOrNull()
 
-        if (selectBurgerNumber == 0)
-            return main()
-    }
-}
-class FrozenCustard() {
-    fun selectFrozenCustardNumber() {
-        println("--------------------[Frozen Custard}--------------------")
-        println("1. Shake   | W 5.9 | 바닐라,초콜렛,솔티드카라멜,블랙&화이트 ")
-        println("2. Cups&Cones   | W 4.9  | 바닐라,초콜렛 ")
-        println("3. Concrete | W 5.9 | 쉐이크쉑의 쫀득한 커스터드와 다양한 믹스-인 조합")
-        println("")
-        println("0. 뒤로 가기 ㅣ 메인 화면으로 가기")
-        var selectFrozenCustardNumber = readLine()?.toIntOrNull()
+fun inputMyInfo(orderManager: OrderManager) {
 
-        if (selectFrozenCustardNumber == 0)
-            return main()
-    }
-}
-class Drinks() {
-    fun selectDrinksNumber() {
-        println("--------------------[Drinks}--------------------")
-        println("1. Lemonade   | W 3.9 | 매장에서 직접 만드는 상큼한 레몬에이드")
-        println("2. IceTea    | W 3.4 | 직접 유기농 홍차를 우려낸 아이스티")
-        println("3. Fifty/Fifty | W 3.5 | 레몬에이드와 아이스티의 만남")
-        println("4. Fountain Soda  | W 3.7 | 코카콜라,스프라이트,환타")
-        println("")
-        println("0. 뒤로 가기 ㅣ 메인 화면으로 가기")
-        var selectDrinksNumber = readLine()?.toIntOrNull()
+    when (inputInfo("selectNumber")) {
 
-        if (selectDrinksNumber == 0)
-            return main()
-    }
-}
-class Beer() {
-    fun selectBeerNumber() {
-        println("--------------------[Beer}--------------------")
-        println("1. ShackMeister Ale   | W 9.8 | 쉐이크쉑의 버거를 위해 특별히 양조한 에일 맥주")
-        println("2. Magpie Brewing Co.   | W 6.8 ")
-        println("3. The Hand and Malt | W 6.8  ")
-        println("")
-        println("0. 뒤로 가기 ㅣ 메인 화면으로 가기")
-        var selectBeerNumber = readLine()?.toIntOrNull()
-
-        if (selectBeerNumber == 0)
-            return main()
+        1 -> burgerList(orderManager)
+        2 -> frozenCustardList(orderManager)
+        3 -> drinksList(orderManager)
+        4 -> beersList(orderManager)
+        0 -> {
+            println("종료합니다.")
+            exitProcess(0)
+        }
+        else -> println("올바른 메뉴 번호를 선택 하세요.")
     }
 }
--- app/src/main/java/com/example/kiosk/menu.kt
+++ app/src/main/java/com/example/kiosk/menu.kt
@@ -1,193 +1,177 @@
-package com.example.kiosk_final
+package com.example.kiosk
 
-import Order
-import com.example.kiosk.main
+open class Menu(val name: String, val price: Int, val description: String)
 
-open class Menu(val name: String, val price: Double, val display: String) {
-    override fun toString(): String {
-        return "$name | W$price | $display"
-    }
-}
+class Burger(name: String, price: Int, description: String) : Menu(name, price, description)
+class FrozenCustard(name: String, price: Int, description: String) : Menu(name, price, description)
+class Drinks(name: String, price: Int, description: String) : Menu(name, price, description)
+class Beers(name: String, price: Int, description: String) : Menu(name, price, description)
 
-class Burger(name: String, price: Double, display: String) : Menu(name, price, display)
-class FrozenCustard(name: String, price: Double, display: String) : Menu(name, price, display)
-class Drink(name: String, price: Double, display: String) : Menu(name, price, display)
-class Beer(name: String, price: Double, display: String) : Menu(name, price, display)
-
-
-fun burger() {
+fun burgerList(orderManager: OrderManager) {
 
     val burgerList = ArrayList<Burger>()
 
-    val shackBurger = Burger("shakeshack", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거")
-    val smokeShack = Burger("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거")
-    val shroomBurger = Burger("Shroom Burger", 9.4, "몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거")
-    val cheeseburger = Burger("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거")
-    burgerList.add(shackBurger)
+    val shakeShackBurger = Burger("Shake Shack Burger", 6900, "토마토, 양상추, 쉑소스가 토핑된 치즈버거")
+    val smokeShack = Burger("Smoke Shack", 8900, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거")
+    val shroomBurger = Burger("Shroom Burger", 9400, "몬스터 치즈와 체다 치즈로 속을 채운 베지테리안 버거")
+    val cheeseBurger = Burger("Cheese Burger", 6900, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거")
+    val hamBurger = Burger("HamBurger", 5400, "비프패티를 기반으로 야채가 들어간 기본버거")
+    burgerList.add(shakeShackBurger)
     burgerList.add(smokeShack)
     burgerList.add(shroomBurger)
-    burgerList.add(cheeseburger)
+    burgerList.add(cheeseBurger)
+    burgerList.add(hamBurger)
 
-    println("--------------------[Burgers}--------------------")
+    println("[ Burgers Menu ]")
     for ((index, burger) in burgerList.withIndex()) {
-        println("[${index + 1}] ${burger.name} | W${burger.price} ㅣ ${burger.display}")
+        println("[${index + 1}] ${burger.name}  |  W ${burger.price}  |  ${burger.description}")
     }
-        println("")
-        println("0. 메인메뉴로 돌아가기")
-        println("")
-        println("번호를 입력해 주세요")
-        while (true) {
-            try {
-                when (val selectNumber = readLine()?.toIntOrNull()) {
-                    in 1..burgerList.size -> {
-                        val selectedBurger = burgerList[selectNumber!! - 1]
-                        println("고르신 것은 ${selectedBurger.name} 입니다.")
-                    }
-
-                    0 -> return main()
-                    else -> {
-                        println("올바른 메뉴 번호를 선택하세요.")
-                    }
-                }
-            } catch (e: Exception) {
-                println("올바른 메뉴 번호를 선택 하세요.")
-            }
-        }
-    }
-
-
-fun frozenCustard() {
-
-    val frozenCustardList = ArrayList<FrozenCustard>()
-
-    val shake = FrozenCustard("shakeshack", 5.9, "바닐라,초콜렛,솔티드카라멜,블랙&화이트")
-    val cupsCones = FrozenCustard("Cups&Cones", 4.9, "바닐라,초콜렛")
-    val concrete = FrozenCustard("Shroom Burger", 5.9, "쉐이크쉑의 쫀득한 커스터드와 다양한 믹스-인 조합")
-    frozenCustardList.add(shake)
-    frozenCustardList.add(cupsCones)
-    frozenCustardList.add(concrete)
-
-    println("--------------------[Frozen Custards}--------------------")
-    for ((index, frozenCustard) in frozenCustardList.withIndex()) {
-        println("[${index + 1}] ${frozenCustard.name} | W${frozenCustard.price} ㅣ ${frozenCustard.display}")
-    }
-        println("")
-        println("0. 메인메뉴로 돌아가기")
-        println("")
-        println("번호를 입력해 주세요")
-        while (true) {
-            try {
-                when (val selectNumber = readLine()?.toIntOrNull()) {
-                    in 1..frozenCustardList.size -> {
-                        val selectedFrozenCustard = frozenCustardList[selectNumber!! - 1]
-                        println("고르신 것은 ${selectedFrozenCustard.name} 입니다.")
-                    }
-
-                    0 -> return main()
-                    else -> {
-                        println("올바른 메뉴 번호를 선택하세요.")
-                    }
-                }
-            } catch (e: Exception) {
-                println("올바른 메뉴 번호를 선택 하세요.")
-            }
-        }
-    }
-
-fun drink() {
-
-    val drinkList = ArrayList<Drink>()
-
-    val lemonade = Drink("Lemonade", 3.9, "매장에서 직접 만드는 상큼한 레몬에이드")
-    val iceTea = Drink("IceTea", 3.4, "직접 유기농 홍차를 우려낸 아이스티")
-    val fiftyFifty = Drink("Fifty/Fifty", 3.5, "레몬에이드와 아이스티의 만남")
-    val fountainSoda = Drink("Fountain Soda", 3.7, "코카콜라,스프라이트,환타")
-    drinkList.add(lemonade)
-    drinkList.add(iceTea)
-    drinkList.add(fiftyFifty)
-    drinkList.add(fountainSoda)
-
-    println("--------------------[Drinks}--------------------")
-    for ((index, drink) in drinkList.withIndex()) {
-        println("[${index + 1}] ${drink.name} | W${drink.price} ㅣ ${drink.display}")
-    }
-        println("")
-        println("0. 메인메뉴로 돌아가기")
-        println("")
-        println("번호를 입력해 주세요")
-        while (true) {
-            try {
-                when (val selectNumber = readLine()?.toIntOrNull()) {
-                    in 1..drinkList.size -> {
-                        val selectedDrink = drinkList[selectNumber!! - 1]
-                        println("고르신 것은 ${selectedDrink.name} 입니다.")
-                    }
-
-                    0 -> return main()
-                    else -> {
-                        println("올바른 메뉴 번호를 선택하세요.")
-                    }
-                }
-            } catch (e: Exception) {
-                println("올바른 메뉴 번호를 선택 하세요.")
-            }
-        }
-    }
-
-fun beer() {
-
-    val beerList = ArrayList<Beer>()
-
-    val shackMeister = Beer("ShackMeister Ale", 9.8, "쉐이크쉑의 버거를 위해 특별히 양조한 에일 맥주")
-    val cupsMagpiebrewing = Beer("Cups&Magpie Brewing Co.", 6.8, "")
-    val handMalt = Beer("The Hand and Malt", 6.8, "")
-    beerList.add(shackMeister)
-    beerList.add(cupsMagpiebrewing)
-    beerList.add(handMalt)
-
-    println("--------------------[Beers}--------------------")
-    for ((index, beer) in beerList.withIndex()) {
-        println("[${index + 1}] ${beer.name} | W${beer.price} ㅣ ${beer.display}")
-    }
-        println("")
-        println("0. 메인메뉴로 돌아가기")
-        println("")
-        println("번호를 입력해 주세요")
-        while (true) {
-            try {
-                when (val selectNumber = readLine()?.toIntOrNull()) {
-                    in 1..beerList.size -> {
-                        val selecteBeer = beerList[selectNumber!! - 1]
-                        println("고르신 것은 ${selecteBeer.name} 입니다.")
-                    }
-
-                    0 -> return main()
-                    else -> {
-                        println("올바른 메뉴 번호를 선택하세요.")
-                    }
-                }
-            } catch (e: Exception) {
-                println("올바른 메뉴 번호를 선택 하세요.")
-            }
-        }
-    }
-
-fun processPayment(order: Order) {
-    println("---- 결제 화면 ----")
-    order.printOrderReceipt()
+    println("[0] 뒤로 가기")
 
     while (true) {
-        println("결제하시려면 금액을 입력해주세요.")
-        val payment = readLine()?.toDoubleOrNull()
+        try {
+            when (val selectNumber = readLine()?.toIntOrNull()) {
+                in 1..burgerList.size -> {
+                    val selectedBurger = burgerList[selectNumber!! - 1]
+                    orderManager.addToOrder(selectedBurger)
+                }
 
-        if (payment != null && payment >= order.getTotalAmount()) {
-            val change = payment - order.getTotalAmount()
-            println("결제가 완료되었습니다. 거스름돈은 W$change 입니다.")
-            order.clearOrder()
-            break
-        } else {
-            println("입력한 금액이 부족합니다. 다시 시도해주세요.")
+                0 -> {
+                    inputMyInfo(orderManager)
+                }
+
+                else -> {
+                    println("올바른 메뉴 번호를 선택하세요.")
+                }
+            }
+        } catch (e: Exception) {
+            println("올바른 메뉴 번호를 선택 하세요.")
         }
     }
 }
 
+fun frozenCustardList(orderManager: OrderManager) {
+
+    val frozenCustardList = ArrayList<FrozenCustard>()
+
+    val shakes = FrozenCustard("Shakes", 5900, "바닐라 쉐이크")
+    val shakeOfTheWeek = FrozenCustard("Shake Of The Week", 6500, "특별한 커스터드 플레이버")
+    val redBeanShake = FrozenCustard("Red Bean Shake", 6500, "신선한 커스터드와 함께 우유와 레드빈이 블렌딩 된 시즈널 쉐이크")
+    val floats = FrozenCustard("Floats", 5900, "루트 비어")
+    val cupsAndCones = FrozenCustard("Cups and Cones", 4900, "바닐라 아이스크림")
+    val concretes = FrozenCustard("Concretes", 5900, "쉐이크쉑의 쫀득한 커스터드와 다양한 믹스-인의 조합")
+    frozenCustardList.add(shakes)
+    frozenCustardList.add(shakeOfTheWeek)
+    frozenCustardList.add(redBeanShake)
+    frozenCustardList.add(floats)
+    frozenCustardList.add(cupsAndCones)
+    frozenCustardList.add(concretes)
+
+    println("[ FrozenCustard Menu ]")
+    for ((index, frozenCustard) in frozenCustardList.withIndex()) {
+        println("[${index + 1}] ${frozenCustard.name}  |  W ${frozenCustard.price}  |  ${frozenCustard.description}")
+    }
+    println("[0] 뒤로 가기")
+    while (true) {
+        try {
+            when (val selectNumber = readLine()?.toIntOrNull()) {
+                in 1..frozenCustardList.size -> {
+                    val selectedFrozenCustard = frozenCustardList[selectNumber!! - 1]
+                    orderManager.addToOrder(selectedFrozenCustard)
+                }
+
+                0 -> {
+                    inputMyInfo(orderManager)
+                }
+
+                else -> {
+                    println("올바른 메뉴 번호를 선택하세요.")
+                }
+            }
+        } catch (e: Exception) {
+            println("올바른 메뉴 번호를 선택 하세요.")
+        }
+    }
+}
+
+fun drinksList(orderManager: OrderManager) {
+
+    val drinksList = ArrayList<Drinks>()
+
+    val shackMadeLemonade = Drinks("Shack Made Lemonade", 3900, "상큼한 레모네이드")
+    val icedTea = Drinks("Iced Tea", 3400, "직접 유기농 홍차를 우려낸 아이스티")
+    val fifty = Drinks("Fifty Fifty", 3500, "레모네이드와 아이스티의 만남")
+    val fountainSoda = Drinks("Fountain Soda", 2700, "코카콜라")
+    val abitaRootBeer = Drinks("Abita Root Beer", 4400, "청량감 있는 독특한 미국식 무알콜 탄산음료")
+    val bottledWater = Drinks("Water", 1000, "지리산 암반대수층으로 만든 생수")
+    drinksList.add(shackMadeLemonade)
+    drinksList.add(icedTea)
+    drinksList.add(fifty)
+    drinksList.add(fountainSoda)
+    drinksList.add(abitaRootBeer)
+    drinksList.add(bottledWater)
+
+    println("[ Drinks Menu ]")
+    for ((index, drinks) in drinksList.withIndex()) {
+        println("[${index + 1}] ${drinks.name}  |  W ${drinks.price}  |  ${drinks.description}")
+    }
+    println("[0] 뒤로 가기")
+
+    while (true) {
+        try {
+            when (val selectNumber = readLine()?.toIntOrNull()) {
+                in 1..drinksList.size -> {
+                    val selectedDrinks = drinksList[selectNumber!! - 1]
+                    orderManager.addToOrder(selectedDrinks)
+                }
+
+                0 -> {
+                    inputMyInfo(orderManager)
+                }
+
+                else -> {
+                    println("올바른 메뉴 번호를 선택하세요.")
+                }
+            }
+        } catch (e: Exception) {
+            println("올바른 메뉴 번호를 선택 하세요.")
+        }
+    }
+}
+fun beersList(orderManager: OrderManager) {
+
+    val beersList = ArrayList<Beers>()
+
+    val shackMeisterAle = Beers("Shack Meister Ale", 9800, "뉴욕 브루클린 브루어리에서 양조한 에일 맥주")
+    val magpieBrewingCo = Beers("Magpie Brewing Co.", 6800, "Pale Ale, Draft")
+
+    beersList.add(shackMeisterAle)
+    beersList.add(magpieBrewingCo)
+
+    println("[ Beers Menu ]")
+    for ((index, beers) in beersList.withIndex()) {
+        println("[${index + 1}] ${beers.name}  |  W ${beers.price}  |  ${beers.description}")
+    }
+    println("[0] 뒤로 가기")
+
+    while (true) {
+        try {
+            when (val selectNumber = readLine()?.toIntOrNull()) {
+                in 1..beersList.size -> {
+                    val selectedBeers = beersList[selectNumber!! - 1]
+                    orderManager.addToOrder(selectedBeers)
+                }
+
+                0 -> {
+                    inputMyInfo(orderManager)
+                }
+
+                else -> {
+                    println("올바른 메뉴 번호를 선택하세요.")
+                }
+            }
+        } catch (e: Exception) {
+            println("올바른 메뉴 번호를 선택 하세요.")
+        }
+    }
+}
